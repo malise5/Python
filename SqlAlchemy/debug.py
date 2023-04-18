aa313ecf8ea6dbf7bff351a20ecb4a234f224113
Halkano Malise <kudezx@gmail.com>
2023-04-18 20:13:26 +0300

    updated

--- SqlAlchemy/debug.py
+++ SqlAlchemy/debug.py
@@ -1,6 +1,4 @@
-from pet import Pet, CONN, CURSOR
-from owner import Owner, CONN, CURSOR
-# from cat import Cat
+
 import ipdb
 
 from sqlalchemy import create_engine
@@ -9,4 +7,27 @@ from sqlalchemy.orm import sessionmaker
 from models import (Base, Pet)
 
 if __name__ == '__main__':
-    
+    # creating Engine
+    engine = create_engine('sqlite:///pet_app.db')
+    Base.metadata.create_all(engine)
+
+    # creating session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # creating a pet and seve it to the database with 'session.add() and session.commit()'
+    rose = Pet(name='rose', species='cat', breed='domestic longhair',
+               temperament='relaxed', owner_id=1)
+    spot = Pet(name='spot', species='dog', breed='husky',
+               temperament='joyful', owner_id=2)
+
+    # PERSSISTING INDIVIDUAL INSTANCE TO DB
+    # session.add(rose)
+    # session.commit()
+
+    # PERSSISTING MULTIPLE INSTANCE TO DB
+    session.bulk_save_objects([rose, spot])
+    session.commit()
+
+
+ipdb.set_trace()
